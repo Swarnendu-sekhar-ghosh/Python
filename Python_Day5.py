4d0cc8b6a6aca3b632f24b406be70ab21f5b25cf
Swarnendu Sekhar Ghosh <swarnendughosh92@gmail.com>
2022-07-06 00:28:16 +0530

    ssg_day5

---
+++ Python_Day5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  5 23:53:44 2022
+
+@author: swarnendu
+"""
+
+
+# Special Operators
+
+word="swarnendu"
+
+for item in enumerate(word):
+    print(item)
+    
+    
+    
